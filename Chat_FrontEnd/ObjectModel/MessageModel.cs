6b51e41f9246c8a16f195b01c1ce9c14f983b703
proxrustes <wifeofwriter@gmail.com>
2022-09-03 15:03:46 +0200

    added jquery

--- Chat_FrontEnd/ObjectModel/MessageModel.cs
+++ Chat_FrontEnd/ObjectModel/MessageModel.cs
@@ -7,10 +7,8 @@
 
         public int chatId { get; set; }
         public int userId { get; set; }
-
         public string messageBody { get; set; }
         public DateTime messageDate { get; set; }
-
         public bool deletedForSelf { get; set; }
     }
 }
